6e9c6f62dbafda53fd03ba3784bbb5414548c71d
Sergey Radyukhin <22dollars@mail.ru>
2022-10-18 18:03:13 +0300

    add keyboard

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+from telegram import ReplyKeyboardMarkup
 from settings import API_KEY
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
@@ -8,7 +8,8 @@ logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def greet_user(update, context):
     print('/start called')
-    update.message.reply_text('Добрый день!')
+    my_keyboard = ReplyKeyboardMarkup([['Где найти артикул?']])
+    update.message.reply_text('Добрый день!', reply_markup=my_keyboard)
 
 
 def talk_to_me(update, context):
@@ -16,13 +17,22 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+
+def send_articul_example(update, context):
+    chat_id = update.effective_chat.id
+    articul_pic_filename = 'images/find_articul.png'
+    context.bot.send_photo(chat_id=chat_id, photo=open(articul_pic_filename, 'rb'))
+    
+
+
 def main():
     mybot = Updater(API_KEY,
                     use_context=True)
     db = mybot.dispatcher
     db.add_handler(CommandHandler('start', greet_user))
+    db.add_handler(CommandHandler('articul', send_articul_example))
+    db.add_handler(MessageHandler(Filters.regex('^(Где найти артикул\?)$'), send_articul_example))
     db.add_handler(MessageHandler(Filters.text, talk_to_me))
-
     logging.info('bot has been started')
     mybot.start_polling()
     mybot.idle()
